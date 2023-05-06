da47189fb2455f507303918cbbe32cdbf73f27cc
Kshitiz Jangid <kshitizjangid01@gmail.com>
2023-05-06 08:47:46 +0530

    First 5 +ve multiple

--- Imp-Ques/multiple.py
+++ Imp-Ques/multiple.py
@@ -1,6 +1,5 @@
 def multi(k:int):
     checker = 0 
-    # breaker = ""
     
     while checker < k:
         li=[]
@@ -9,10 +8,7 @@ def multi(k:int):
         
         print("Program completed.")
         return li
-        # breaker = input("Type 'done' to exit.\n")
-        
-        # if breaker == "done":
-        #     return
+    
     return "Negative value of 'k' is selected. Please select +ve value and try again"
 
 print(multi(int(input("Enter a +ve value of 'k':\n"))))
